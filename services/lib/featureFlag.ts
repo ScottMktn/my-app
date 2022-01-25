f9896dc60237de31503970ec2090d473b85e0e80
Scott Nguyen <s.nguyen@draftkings.com>
2022-01-25 14:01:51 -0500

    lib changes

--- services/lib/featureFlag.ts
+++ services/lib/featureFlag.ts
@@ -2,6 +2,5 @@ import axios from "axios"
 
 export const getFeatureFlags = async () => {
     const res = await axios.get('http://localhost:3000/api/flags');
-    const flags = res.data.flags;
-    return flags;
+    return res.data.flags;
 }
